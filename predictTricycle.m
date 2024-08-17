3c04ceb8c5c65a1b9d824f79d6329caccba29b79
jamila22 <naffati.1762645@studenti.uniroma1.it>
2024-08-17 15:55:54 +0200

    Add new octave dataReader and Tricycle model

--- predictTricycle.m
+++ predictTricycle.m
@@ -1,33 +1,24 @@
+#{Function definition for predictTricycle inspired by: 
+probabilistic_robotics_2023_24/source/octave/19b_mobile_base_calibration/predictTricycle.m #}
+
 function delta_pose = predictTricycle(traction_ticks, steer_ticks, x)
-  k_traction = x(1); 
-  k_steer = x(2);
-  steer_encoder_offset = x(3);
-  baseline = x(4);
+    k_traction = x(1); 
+    k_steer = x(2);
+    steer_encoder_offset = x(3);
+    baseline = x(4);
 
-  traction_linear_offset = k_traction * traction_ticks;
-  fprintf('traction_linear_offset = %.4f\n', traction_linear_offset);
-  
-  steering_angle = k_steer * steer_ticks + steer_encoder_offset;
-  fprintf('steering_angle = %.4f\n', steering_angle);
-  
-  back_wheels_displacement = traction_linear_offset * cos(steering_angle);
-  fprintf('back_wheels_displacement = %.4f\n', back_wheels_displacement);
-  
-  dth = traction_linear_offset * sin(steering_angle) / baseline;
-  fprintf('dth = %.4f\n', dth);
-  
-  drho = back_wheels_displacement;
-  fprintf('drho = %.4f\n', drho);
+    traction_linear_offset = k_traction * traction_ticks;
+    steering_angle = 2 * pi * k_steer * steer_ticks + steer_encoder_offset;
+    back_wheels_displacement = traction_linear_offset * cos(steering_angle);
 
-  S = [0.00000, 0.00833, 0.00000, -0.16667, 0.00000, 1.00000];
-  C = [0.00139, 0.00000, -0.04167, 0.00000, 0.50000, 0.00000];
+    dth = traction_linear_offset * sin(steering_angle) / baseline;
+    drho = back_wheels_displacement;
 
-  dx = drho * polyval(S, dth);
-  fprintf('dx = %.4f\n', dx);
-  
-  dy = drho * polyval(C, dth);
-  fprintf('dy = %.4f\n', dy);
+    S = [0.00000, 0.00833, 0.00000, -0.16667, 0.00000, 1.00000];
+    C = [0.00139, 0.00000, -0.04167, 0.00000, 0.50000, 0.00000];
 
-  delta_pose = [dx; dy; dth];
+    dx = drho * polyval(S, dth);
+    dy = drho * polyval(C, dth);
+
+    delta_pose = [dx; dy; dth];
 endfunction
-
